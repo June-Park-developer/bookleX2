af822d8bc372059d972a2c1f98989e9f89106772
JungEun Park <jungeunp97@naver.com>
2025-09-12 17:27:49 +0900

    [fix] test.yml 에서 migration 제거

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -7,5 +7,5 @@ export const getTypeOrmConfig = (
   type: 'postgres',
   url: configService.get<string>('DATABASE_URL'),
   entities: [__dirname + '/../**/*.entity.{js, ts}'],
-  synchronize: configService.get<string>('NODE_ENV') === 'development',
+  synchronize: configService.get<string>('NODE_ENV') !== 'deploy',
 });
